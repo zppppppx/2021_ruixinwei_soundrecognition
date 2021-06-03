3f1c0cf9985f10dee756ea4384df2dfc08fa7c04
zppppppx <3180105152@zju.edu.cn>
2021-06-03 10:58:12 +0800

    Update utils/sound_preprocess.py

--- src/utils/sound_preprocess.py
+++ src/utils/sound_preprocess.py
@@ -1,26 +1,33 @@
 import numpy as np
-import wave
+from scipy.io import wavfile
+import librosa
 
 
 def sound_load(sound_path, start, end):
     """
     Load sound data from the files with specific segment notated by start and end in time.
+    And then extract the features in mfccs.
 
     Args:
         sound_path: the wav file in need of loading.
-        start: the beginning time of the sound segment.
-        end: the ending time of the sound segment.
+        start: the beginning time of the sound segment with unit sec.
+        end: the ending time of the sound segment with unit sec.
 
     Returns:
-        : a numpy array of sound data
+        mfccsscaled: a numpy array of feature
     """
-    with wave.open(sound_path, "rb") as f:
-        f = wave.open(sound_path)
-        n_channels, sampwidth, framerate, frames, _, _ = f.getparams() # get the params of the file
+    try:
+        sample_rate, sig = wavfile.read(sound_path)
+        start, end = int(start*sample_rate), int(end*sample_rate)
+        segment = sig[start:end] # get the fraction of the sound
+        if segment.dtype == np.int16:
+            bitwidth = 16
+        segment = segment/(2.0**bitwidth)
+        mfccs = librosa.feature.mfcc(y=segment, sr=sample_rate, n_mfcc=24)
+        mfccsscaled = np.mean(mfccs.T,axis=0)
 
-        start, end = start*framerate, end*framerate
-        f.setpos(start) # set the beginning pointer
-        data = f.readframes(end-start)
+    except Exception as e:
+        print("Error encountered while parsing file: ", sound_path)
+        return None
     
-    data = np.fromstring(data, dtype=np.short)
-    return data
+    return mfccsscaled
