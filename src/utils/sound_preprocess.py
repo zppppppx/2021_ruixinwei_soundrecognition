44648811536bccd3e926697aa2d33a5afbabde0a
zppppppx <3180105152@zju.edu.cn>
2021-06-03 11:10:14 +0800

    Update utils/sound_preprocess.py

--- src/utils/sound_preprocess.py
+++ src/utils/sound_preprocess.py
@@ -1,4 +1,5 @@
 import numpy as np
+from numpy.lib.function_base import extract
 from scipy.io import wavfile
 import librosa
 
@@ -6,7 +7,7 @@ import librosa
 def sound_load(sound_path, start, end):
     """
     Load sound data from the files with specific segment notated by start and end in time.
-    And then extract the features in mfccs.
+    And then extract the features of mfccs.
 
     Args:
         sound_path: the wav file in need of loading.
@@ -17,12 +18,17 @@ def sound_load(sound_path, start, end):
         mfccsscaled: a numpy array of feature
     """
     try:
+        # get the fraction of the sound
         sample_rate, sig = wavfile.read(sound_path)
         start, end = int(start*sample_rate), int(end*sample_rate)
-        segment = sig[start:end] # get the fraction of the sound
-        if segment.dtype == np.int16:
-            bitwidth = 16
+        segment = sig[start:end] 
+        
+        # unify the sound to [0, 1]
+        sampwidth = [np.int8, np.int16, np.int32]
+        bitwidth = sampwidth.index(segment.dtype) + 1
         segment = segment/(2.0**bitwidth)
+
+        # extract the mfcc feature
         mfccs = librosa.feature.mfcc(y=segment, sr=sample_rate, n_mfcc=24)
         mfccsscaled = np.mean(mfccs.T,axis=0)
 
