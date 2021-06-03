bc1cf24de02be88c500d0f16ec6a5ac4a6f35265
zppppppx <3180105152@zju.edu.cn>
2021-06-03 12:34:11 +0800

    Update utils/data_preprocess.py adding csv file resolution(not finished)

--- src/utils/data_preprocess.py
+++ src/utils/data_preprocess.py
@@ -37,3 +37,18 @@ def sound_load(sound_path, start, end):
         return None
     
     return mfccsscaled
+
+
+def csvfile_resolution(csv_path):
+    """
+    Analyze the csv file aiming to get wav filename 
+    and the corresponding audio segment split up with time mark.
+
+    Args:
+        csv_path: the path of the csv file.
+
+    Return:
+        filename: a list of names of files. eg: [file1, ...]
+        timestamp: a list of timestamp lists marking the start and the end of a segment. eg: [[(s1,e1), (s2,e2)], ...]
+        labels: a list of label lists marking the class corresponding to timestamp. eg: [[class1, class2], ...]
+    """
