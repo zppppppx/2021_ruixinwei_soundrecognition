749bfc028f72d1158d7f0fe506fe2d58d3f19707
zppppppx <3180105152@zju.edu.cn>
2021-06-05 11:13:49 +0800

    Update data_preprocess.py adding feature_extract which aims to extract wav files mfcc features and load into a txt file for training

--- src/utils/data_preprocess.py
+++ src/utils/data_preprocess.py
@@ -1,5 +1,7 @@
+from librosa import feature
 import numpy as np
 from numpy.lib.function_base import extract
+from scipy.fftpack.realtransforms import dst
 from scipy.io import wavfile
 import librosa
 import pandas as pd
@@ -10,7 +12,7 @@ class data_preprocess:
     Define the class of data preprocess to better resolute the data.
     Including sound_load, csv_resolution
     """
-    def  __init__(self, root_path):
+    def  __init__(self, root_path='../data'):
         """
         Initialize the args
 
@@ -20,7 +22,7 @@ class data_preprocess:
         self.root_path = root_path
 
 
-    def sound_load(self, sound_path, start, end):
+    def sound_load(self, sample_rate, sig, start, end):
         """
         Load sound data from the files with specific segment notated by start and end in time.
         And then extract the features of mfccs.
@@ -33,24 +35,18 @@ class data_preprocess:
         Returns:
             mfccsscaled: a numpy array of feature
         """
-        try:
-            # get the fraction of the sound
-            sample_rate, sig = wavfile.read(sound_path)
-            start, end = int(start*sample_rate), int(end*sample_rate)
-            segment = sig[start:end] 
-            
-            # unify the sound to [0, 1]
-            sampwidth = [np.int8, np.int16, np.int32]
-            bitwidth = sampwidth.index(segment.dtype) + 1
-            segment = segment/(2.0**bitwidth)
+        # get the fraction of the sound
+        start, end = int(start*sample_rate), int(end*sample_rate)
+        segment = sig[start:end] 
+        
+        # unify the sound to [0, 1]
+        sampwidth = [np.int8, np.int16, np.int32]
+        bitwidth = sampwidth.index(segment.dtype) + 1
+        segment = segment/(2.0**bitwidth)
 
-            # extract the mfcc feature
-            mfccs = librosa.feature.mfcc(y=segment, sr=sample_rate, n_mfcc=24)
-            mfccsscaled = np.mean(mfccs.T,axis=0)
-
-        except Exception as e:
-            print("Error encountered while parsing file: ", sound_path)
-            return None
+        # extract the mfcc feature
+        mfccs = librosa.feature.mfcc(y=segment, sr=sample_rate, n_mfcc=24)
+        mfccsscaled = np.mean(mfccs.T,axis=0)
         
         return mfccsscaled
 
@@ -90,6 +86,54 @@ class data_preprocess:
         
         return filenames, timestamps, labels
 
+    
+    def feature_extract(self, filenames, timestamps, labels, dst_path, train=True):
+        """
+        Extract the feature of the sound fragment and save it to a txt or csv file.
 
+        Args:
+            train: marking your target dir, True or false
+            filenames: a list of filenames in need of extraction.
+            timestamps: similar to csvfile_resolution.
+            labels: similar to csvfile_resolution.
+            dst_path: the file path to be saved as.
+        """
+        # handle the path issues
+        dir = 'train' if train else 'val'
+        dst_path = os.path.join(self.root_path, dst_path)
+        if os.path.exists(dst_path):
+            os.remove(dst_path)
+
+        with open(dst_path, "a") as f:
+            # walk through all the files
+            cnt = 0
+            for i in range(len(filenames)):
+                filename = filenames[i]
+                sound_path = os.path.join(self.root_path, dir, filename+'.wav')
+                try:
+                    sample_rate, sig = wavfile.read(sound_path)
+                
+                except Exception as e:
+                    print("Error encountered while parsing file: ", sound_path)
+                    return None
+                
+                timestamp = timestamps[i]
+                label = labels[i]
+
+                # walk through all the labels
+                for j in range(len(label)):
+                    mfcc_features = self.sound_load(sample_rate, sig, timestamp[j][0], timestamp[j][1])
+                    classification = label[j]
+                    row_data = str(classification) + '\t'
+                    # elements = [str(element) + ' ' for element in mfcc_features]
+                    for element in mfcc_features:
+                        row_data = row_data + str(element) + ' '
+                    row_data += '\n'
+
+                    f.write(row_data)
+
+                    cnt += 1
+                    if cnt%100 == 0:
+                        print('{} sound fragments processed'.format(cnt))
 
     
